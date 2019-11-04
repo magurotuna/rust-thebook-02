3979390ad90365668f8385a89b68b9926b058317
Yusuke Tanaka <wing0920@gmail.com>
2019-11-04 21:53:07 +0900

    rand

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+extern crate rand;
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is: {}", secret_number);
     println!("Please input your guess.");
 
     let mut guess = String::new();
@@ -9,5 +13,6 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    println!("You guessed: {}", guess);
     println!("You guessed: {}", guess);
 }
